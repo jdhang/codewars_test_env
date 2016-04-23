2b3899e9a24d9fd21aa2382ef965afdb23a7cfcc
Jason Hang <dev.jdhang@gmail.com>
2016-04-23 16:48:19 -0400

    Reset script file to a blank file, changed test file to be generic, updated index.html to load the script and script.test files.

--- script.test.js
+++ script.test.js
@@ -1,20 +1,5 @@
-var tests = [
-  ['1+1', 2],
-  ['1 - 1', 0],
-  ['1* 1', 1],
-  ['1 /1', 1],
-  ['-123', -123],
-  ['123', 123],
-  ['2 /2+3 * 4.75- -6', 21.25],
-  ['12* 123', 1476],
-  ['2 / (2 + 3) * 4.33 - -6', 7.732]
-];
-
-describe('Evaluate Math Expressions', function () {
-  tests.forEach(function (test) {
-    it('should evaluate ' + test[0] + ' to ' + test[1], function () {
-      expect(calc(test[0])).to.eql(test[1]);
-    });
-  })
-
+describe('Script Function', function () {
+  it('should do something', function () {
+    expect(1).to.eql(1);
+  });
 })
